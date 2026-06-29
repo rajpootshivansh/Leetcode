3313ce5529ee0ebc7c09a9b8471265cde607f491
Shivansh Rajpoot <162569534+rajpootshivansh@users.noreply.github.com>
2026-06-29 21:54:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (49.38%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,29 +1,11 @@
-class Solution
-{
-    public:
-
-        int setbits(int n)
-        {
-            int count = 0;
-            while (n)
-            {
-                count += n &1;
-                n >>= 1;
-            }
-            return count;
-        }
-
-    bool isPowerOfTwo(int n)
-    {
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
 
 
 
-if(n<=0) return false;
-        if (setbits(n) == 1)
-        {
-            return true;
-        }
-
-        return false;
+        return n > 0 &&
+       (n & (n - 1)) == 0;
     }
 };
